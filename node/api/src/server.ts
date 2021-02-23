8cbec654e0931af9404457181af4b204d2070848
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-02-22 22:00:14 -0400

    create route get

--- node/api/src/server.ts
+++ node/api/src/server.ts
@@ -2,4 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log("server running"));
+app.get("/", (request, response) => response.json({ message: "server running 🚀" }));
+
+app.listen(3333, () => console.log("server running 🚀"));
