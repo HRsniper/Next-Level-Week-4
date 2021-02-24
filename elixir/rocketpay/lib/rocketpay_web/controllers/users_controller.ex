4fb9b57b63dfc0acba144bd6d93e0b54503518b9
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-02-24 10:26:04 -0400

    lindando com erro patern match

--- elixir/rocketpay/lib/rocketpay_web/controllers/users_controller.ex
+++ elixir/rocketpay/lib/rocketpay_web/controllers/users_controller.ex
@@ -7,20 +7,13 @@ defmodule RocketpayWeb.UsersController do
   action_fallback RocketpayWeb.FallbackController
 
   def create(connection, params) do
-    params
-    |> Rocketpay.create_user()
-    |> handle_response(connection)
+    # caso erro with joga o ero para FallbackController
+    with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
+      connection
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
-
-  defp handle_response({:ok, %User{} = user}, connection) do
-    connection
-    |> put_status(:created)
-    # |> json(%{message: "User #{user}"})
-    # users_view.ex
-    |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, _result} = error, _connection), do: error
 end
 
 # https://hexdocs.pm/phoenix/Phoenix.Controller.html#summary
