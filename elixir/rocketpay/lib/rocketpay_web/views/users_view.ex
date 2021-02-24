b655e9a44a7b17be4cbac614a10c7ac077344fc2
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-02-24 09:04:39 -0400

    fix not hashed pass, and view

--- elixir/rocketpay/lib/rocketpay_web/views/users_view.ex
+++ elixir/rocketpay/lib/rocketpay_web/views/users_view.ex
@@ -3,13 +3,15 @@ defmodule RocketpayWeb.UsersView do
 
   alias Rocketpay.User
 
-  def render("create.json", %{user: %User{ id: id name: name nickname: nickname }}) do
+  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
     # retorno
-    %{message: "User created"},
-    user: %{
-      id: id,
-      name: name,
-      nickname: nickname,
+    %{
+      message: "User created",
+      user: %{
+        id: id,
+        name: name,
+        nickname: nickname
+      }
     }
   end
 end
