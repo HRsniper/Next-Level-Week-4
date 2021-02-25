9ce0ae8255fedfe80d4645afa7ecc2a8ce7c9b1b
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2021-02-25 18:02:26 -0400

    countdown

--- react/moveit-next/src/pages/index.tsx
+++ react/moveit-next/src/pages/index.tsx
@@ -1,16 +1,25 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 
 import styles from "../styles/pages/Home.module.css";
+import Head from "next/head";
 
 function Home() {
   return (
     <main className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
